5adaa7c1d9addb3a0befc2742980c9dd8f08648e
bharat kumar shah <akashche72@gmail.com>
2021-05-04 12:56:18 +0400

    working on dispatching every 2 secounds

--- src/components/DynamicData.tsx
+++ src/components/DynamicData.tsx
@@ -1,50 +1,46 @@
 import React, { useEffect } from 'react';
-import {Button, Container} from './Elements';
-import {useTypedSelector} from '../hooks/use-typed-selector';
-import {useActions} from '../hooks/use-actions';
-import {DynamicData} from '../state/actions';
+import { Button, Container } from './Elements';
+import { useTypedSelector } from '../hooks/use-typed-selector';
+import { useActions } from '../hooks/use-actions';
+import { DynamicData } from '../state/actions';
 
 function App() {
     const precision = 100; // 2 decimals
-    const getDynamicData = useTypedSelector(({dynamicData}) => {
+    const getDynamicData = useTypedSelector(({ dynamicData }) => {
         return dynamicData;
     });
-    const {addDynamicData} = useActions();
+    console.log(getDynamicData);
+  
+    const { addDynamicData } = useActions();
 
     useEffect(() => {
-        // addDynamicData({
-        //     filedName: "My Name",
-        //     value: 3,
-        //     mutable: false,
-        //     previousValue: 3,
-        //     enabled: true,
-        // })
-       setInterval(() => {
-           const getDynamicData = updatedValue([{
-            filedName: "My Name",
-            value: 3,
-            mutable: false,
-            previousValue: 3,
-            enabled: true,
-        }]);
-       // console.log(getDynamicData)
-            
-        }, 2000);
-        
-        //addDynamicData(getDynamicData);
-        
+        let i: number = 0;
+        if(getDynamicData.length === 5) {
+            return;
+        }
+        const timer = setInterval(() => {
+                    addDynamicData({
+                    filedName: "My Name",
+                    value: 3,
+                    mutable: false,
+                    previousValue: 3,
+                    enabled: true,
+                });
+           // i++;
+        }, 3000);
+        return () => clearInterval(timer);
 
     }, [addDynamicData, getDynamicData])
 
-   
 
-    const updatedValue = (fields:DynamicData[]) => {
+
+    const updatedValue = (fields: DynamicData[]) => {
         fields.map((item) => {
             if (item.enabled) {
                 var randomnum =
                     Math.floor(
                         Math.random() * (2 * precision - 1 * precision) +
-                            1 * precision
+                        1 * precision
                     ) /
                     (1 * precision);
                 item.previousValue = item.value;
@@ -52,12 +48,12 @@ function App() {
                 item.upOrDown = item.previousValue > item.value ? "-" : "+";
                 item.time = new Date();
             }
-    
+
             return item;
         });
         return fields;
     };
-   
+    
     return <Container><Button>Hello</Button></Container>
 }
 
--- src/state/reducers/dynamic-data.ts
+++ src/state/reducers/dynamic-data.ts
@@ -1,38 +1,38 @@
 import {Action, ActionTypes} from '../action-types';
 import {DynamicData} from '../actions'
 
-const defaultData = [
-	{
-		filedName: "A",
-		value: 3,
-		mutable: false,
-		previousValue: 3,
-		enabled: true,
-	},
-	{
-		filedName: "B",
-		value: 3,
-		mutable: false,
-		previousValue: 3,
-		enabled: true,
-	},
-	{
-		filedName: "C",
-		value: 3,
-		mutable: false,
-		previousValue: 3,
-		enabled: true,
-	},
-	{
-		filedName: "D",
-		value: 3,
-		mutable: false,
-		previousValue: 3,
-		enabled: false,
-	},
-];
+// const defaultData = [
+// 	{
+// 		filedName: "A",
+// 		value: 3,
+// 		mutable: false,
+// 		previousValue: 3,
+// 		enabled: true,
+// 	},
+// 	{
+// 		filedName: "B",
+// 		value: 3,
+// 		mutable: false,
+// 		previousValue: 3,
+// 		enabled: true,
+// 	},
+// 	{
+// 		filedName: "C",
+// 		value: 3,
+// 		mutable: false,
+// 		previousValue: 3,
+// 		enabled: true,
+// 	},
+// 	{
+// 		filedName: "D",
+// 		value: 3,
+// 		mutable: false,
+// 		previousValue: 3,
+// 		enabled: false,
+// 	},
+// ];
 
-export const dymaicDataReducers = (state: DynamicData[] = defaultData, action:Action) => {
+export const dymaicDataReducers = (state: DynamicData[] = [], action:Action) => {
     switch(action.type) {
         case ActionTypes.addDynamicData:
             return [...state, action.payload];
