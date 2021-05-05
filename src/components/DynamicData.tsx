90d6d2c40db41e2d3436cdc4d8f1c76f1a215ba5
bharat kumar shah <akashche72@gmail.com>
2021-05-05 13:51:03 +0400

    create different reducer for storing datathere is some issue while dispatching the data

--- src/components/DynamicData.tsx
+++ src/components/DynamicData.tsx
@@ -1,40 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Button, Container } from './Elements';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import { useActions } from '../hooks/use-actions';
 import { DynamicData } from '../state/actions';
+const defaultData = [
+    {
+        filedName: "A",
+        value: 3,
+        mutable: false,
+        previousValue: 3,
+        enabled: true,
+    },
+    {
+        filedName: "B",
+        value: 3,
+        mutable: false,
+        previousValue: 3,
+        enabled: true,
+    },
+    {
+        filedName: "C",
+        value: 3,
+        mutable: false,
+        previousValue: 3,
+        enabled: true,
+    },
+    {
+        filedName: "D",
+        value: 3,
+        mutable: false,
+        previousValue: 3,
+        enabled: false,
+    },
+];
 
 function App() {
-    const precision = 100; // 2 decimals
+    const [i, setI] = useState<number>(0);
+    const precision = 100;
+
+  
+
     const getDynamicData = useTypedSelector(({ dynamicData }) => {
         return dynamicData;
     });
-    console.log(getDynamicData);
-  
+   console.log(getDynamicData);
     const { addDynamicData } = useActions();
 
-    useEffect(() => {
-        let i: number = 0;
-        if(getDynamicData.length === 5) {
-            return;
-        }
-        const timer = setInterval(() => {
-                    addDynamicData({
-                    filedName: "My Name",
-                    value: 3,
-                    mutable: false,
-                    previousValue: 3,
-                    enabled: true,
-                });
-           // i++;
-        }, 3000);
-        return () => clearInterval(timer);
-
-    }, [addDynamicData, getDynamicData])
-
-
-
-    const updatedValue = (fields: DynamicData[]) => {
+    const updatedValue = useCallback((fields: DynamicData[]) => {
         fields.map((item) => {
             if (item.enabled) {
                 var randomnum =
@@ -48,13 +60,34 @@ function App() {
                 item.upOrDown = item.previousValue > item.value ? "-" : "+";
                 item.time = new Date();
             }
-
             return item;
         });
         return fields;
-    };
+    }, []);
+ 
+    useEffect(() => {   
+        if (getDynamicData.length === 5 || i === 5) {
+            return;
+        }
+        const timer = setInterval(() => {
+            const updatedDate = updatedValue(defaultData)
+            // const updatedData = addDynamicData({
+            //     filedName: "A",
+            //     value: 3,
+            //     mutable: false,
+            //     previousValue: 3,
+            //     enabled: true,
+            // });
+            console.table(updatedDate);
+            //addDynamicData(updatedDate);
+            
+            setI(i+1);
+           
+        }, 1000);
+        return () => clearInterval(timer);
+    }, [addDynamicData, getDynamicData, i, updatedValue])
+
     
     return <Container><Button>Hello</Button></Container>
 }
-
 export default App;
